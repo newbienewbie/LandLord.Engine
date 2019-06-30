649e5bb87eec9c71fa269156e3bf461b7aceb69c
newbienewbie <itminus@hotmail.com>
2019-07-01 07:26:47 +0800

    update  elmish to use a flat model

--- LandLord.ElmishApp/Program.fs
+++ LandLord.ElmishApp/Program.fs
@@ -9,12 +9,12 @@ open System.Collections.Generic
 type Player = 
    {
        name: string;
-       cards: PlayingCard list;
    }
 
 type Model = 
     {
         players: Player list;
+        cards: (PlayingCard list) list;
         prevTurn: int;
         prevCards: PlayingCard list;
     }
@@ -24,109 +24,98 @@ type Msg =
     | AddUserSucceed of Player
     | AddUserFail of Player
     | Deal of PlayingCard list * PlayingCard list * PlayingCard list
+    | AppendCards of int * PlayingCard list
     | StartPlayingCard of PlayingCard list
     | PlayCard of int * PlayingCard list
     | PlayCardSucceed of int * PlayingCard list
     | PlaycardFail of int * PlayingCard list
-    | Win 
+    | Win of int
 
 
 let init () =
-    let cards1 = 
-        [ 
-            NormalCard(CardValue.Ace, Suit.Diamond); 
-            NormalCard(CardValue.Ace, Suit.Club); 
-            NormalCard(CardValue.Ace, Suit.Heart); 
-            NormalCard(CardValue.Ace, Suit.Spade); 
-            NormalCard(CardValue.Three, Suit.Spade); 
-            NormalCard(CardValue.Three, Suit.Club); 
-            NormalCard(CardValue.Two, Suit.Spade); 
-            NormalCard(CardValue.King, Suit.Spade); 
-        ]
-    let cards2 = 
-        [ 
-            NormalCard(CardValue.Ten, Suit.Diamond); 
-            NormalCard(CardValue.Ten, Suit.Club); 
-            NormalCard(CardValue.Ten, Suit.Heart); 
-            NormalCard(CardValue.Ten, Suit.Spade); 
-            NormalCard(CardValue.Three, Suit.Diamond); 
-            NormalCard(CardValue.Three, Suit.Heart); 
-            NormalCard(CardValue.Two, Suit.Club); 
-            NormalCard(CardValue.Queen, Suit.Heart); 
-        ]
 
-    let cards3 = 
-        [ 
-            NormalCard(CardValue.Jack, Suit.Heart); 
-        ]
+    let nth = 2  // 0,1,2 : the landloard
+
+    let cards = Facade.createFullCards() |> Facade.shuffle
+    let (reserved, (cards1, cards2, cards3)) = Facade.deal cards
 
     {
         players = [];
+        cards = [];
         prevTurn = -1;
         prevCards = [];
     },
     Cmd.batch 
-        [ AddUser({ name="player1"; cards =[] }) |> Cmd.ofMsg ;
-          AddUser({ name="player2"; cards =[] }) |> Cmd.ofMsg ;
-          AddUser({ name="player3"; cards =[] }) |> Cmd.ofMsg ;
-          Deal(cards1, cards2, cards3) |> Cmd.ofMsg ; 
+        [ AddUser({ name="player1" }) |> Cmd.ofMsg;
+          AddUser({ name="player2" }) |> Cmd.ofMsg;
+          AddUser({ name="player3" }) |> Cmd.ofMsg;
+          Deal(cards1, cards2, cards3) |> Cmd.ofMsg; 
+          AppendCards(nth, reserved) |> Cmd.ofMsg;
           StartPlayingCard([NormalCard(CardValue.King, Suit.Spade)]) |> Cmd.ofMsg 
         ]
 
 let update msg state = 
+    let addCardsForNth nth (cards: PlayingCard list) = 
+        let cards = 
+            state.cards
+            |> List.mapi (fun i v -> if i = nth then v @ cards else v )
+        { 
+           state with cards = cards; 
+        }
+
+    let _playCardsNth nth (cards: PlayingCard list) = 
+        let cards = 
+            state.cards
+            |> List.mapi (fun i v -> 
+                let remainingCards =
+                    Set.ofList state.cards.[nth] - Set.ofList cards
+                    |> Set.toList
+                if i = nth then remainingCards else v 
+            )
+        { 
+           state with cards = cards; prevTurn = nth;
+        }
+
     match msg with
     | AddUser player ->
-        if List.length state.players < 4 then 
+        if List.length state.players < 3 then 
             { state with players = state.players @[player] },
             AddUserSucceed player |> Cmd.ofMsg
         else 
             state, AddUserFail player |> Cmd.ofMsg
     | Deal (cards1, cards2, cards3) ->
-        let players= 
-            List.zip state.players [cards1; cards2; cards3]
-            |> List.map (fun z -> 
-                let player, cards = z
-                {player with cards= cards; }
-            )
         { 
-           state with players = players ; 
+           state with cards = [cards1; cards2; cards3]; 
         },
+        Cmd.none
+    | AppendCards (nth, cards) ->
+        addCardsForNth nth cards,
         Cmd.none
     | StartPlayingCard cards ->
         let turn = 0
-        let _players = 
-            let currentPlayer = state.players.[turn]
-            let currentPlayerRawCards = currentPlayer.cards
-            let remainingCards = 
-                (Set.ofList currentPlayerRawCards) - (Set.ofList cards)
-                |> List.ofSeq
-                |> sort
-            state.players 
-            |> List.mapi (fun i v -> 
-                if i = turn then { v with cards = remainingCards} else v
-            )
-        { state with prevTurn = 0; players = _players },
-        PlayCardSucceed(turn,cards) |> Cmd.ofMsg
-    | PlayCard (turn, cards) ->
         match Facade.canPlay (state.prevCards) cards with
         | true ->
-            let _players = 
-                let currentPlayer = state.players.[turn]
-                let currentPlayerRawCards = currentPlayer.cards
-                let newCards = 
-                    (Set.ofList currentPlayerRawCards) - (Set.ofList cards)
-                    |> List.ofSeq
-                    |> sort
-                state.players 
-                |> List.mapi (fun i v -> 
-                    if i = turn then { v with cards = newCards} else v
-                )
-            { state with prevTurn = state.prevTurn + 1; players = _players },
+            _playCardsNth turn cards,
             PlayCardSucceed(turn,cards) |> Cmd.ofMsg
         | false -> 
             state, 
             PlaycardFail(turn,cards) |> Cmd.ofMsg
-    | Win -> state, Cmd.none
+    | PlayCard (turn, cards) ->
+        match Facade.canPlay (state.prevCards) cards with
+        | true ->
+            _playCardsNth turn cards,
+            PlayCardSucceed(turn,cards) |> Cmd.ofMsg
+        | false -> 
+            state, 
+            PlaycardFail(turn,cards) |> Cmd.ofMsg
+    | PlayCardSucceed (nth, cards) ->
+        let remainings = state.cards.[nth] |> List.length 
+        if remainings = 0 then 
+            state,
+            Win nth |> Cmd.ofMsg 
+        else
+            state, Cmd.none
+    | Win nth -> state, Cmd.none
     | _ -> state,Cmd.none
 
 
