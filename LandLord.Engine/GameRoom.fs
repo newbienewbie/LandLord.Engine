77442ead0fc7010f7d88dba4318165aad96e2678
newbienewbie <itminus@hotmail.com>
2019-07-15 21:01:50 +0800

    Add GameRoom class & unit test for using in C#

---
+++ LandLord.Engine/GameRoom.fs
@@ -0,0 +1,152 @@
+﻿namespace Itminus.LandLord.Engine
+
+open System
+open System.Collections.Generic
+open Card
+open System.Linq
+
+type Player()= 
+    member val Name:string = "" 
+        with get,set
+
+/// a POCO that describes the meta data of GameRoom
+type IGameRoomMetaData = 
+    abstract member Id :Guid with get
+    abstract member Players: IList<Player> with get
+    abstract member LandLordIndex: int with get
+    abstract member CurrentTurn: int with get
+    abstract member PrevIndex: int with get
+    abstract member PrevCards: IList<PlayingCard> with get
+    abstract member Cards: IList<IList<PlayingCard>> with get
+    abstract member ReservedCards: IList<PlayingCard> with get
+
+type GameRoomMetaData() = 
+
+    let mutable _id:Guid = Guid.NewGuid() 
+    let mutable _landLordIndex: int = -1 
+    let mutable _currentTurn: int = 0 
+    let mutable _prevIndex: int = -1 
+    let mutable _prevCards: IList<PlayingCard> = (new List<PlayingCard>() :> IList<_>) 
+    let mutable _players: IList<Player> = (new List<Player>() :> IList<_>) 
+    let mutable _cards: IList<IList<PlayingCard>> = (new List<IList<PlayingCard>>() :> IList<_>)
+    let mutable _reservedCards: IList<PlayingCard> = (new List<PlayingCard>() :> IList<_>)
+   
+
+    do 
+        // initialize the _cards with 3 emtpy cards list
+        _cards.Add(new List<PlayingCard>())
+        _cards.Add(new List<PlayingCard>())
+        _cards.Add(new List<PlayingCard>())
+
+    member this.Id with set (v: Guid) = _id <- v
+    member this.Cards with set (v: IList<IList<PlayingCard>>) = _cards <- v
+    member this.Players with set (v: IList<Player>) =  _players <- v
+    member this.ReservedCards with internal set(v: IList<PlayingCard>) = _reservedCards <- v
+
+    member this.CurrentTurn with set (v: int) =  _currentTurn <- v
+    member this.LandLordIndex with set (v: int) = _landLordIndex <- v
+
+    member this.PrevCards with set (v: IList<PlayingCard>) = _prevCards <- v
+    member this.PrevIndex with set (v: int) = _prevIndex <- v
+
+    interface IGameRoomMetaData with
+        member this.Id with get (): Guid = _id
+        member this.Cards with get (): IList<IList<PlayingCard>> = _cards
+        member this.CurrentTurn with get (): int = _currentTurn
+        member this.LandLordIndex with get (): int = _landLordIndex
+        member this.PrevCards with get(): IList<PlayingCard> = _prevCards 
+        member this.PrevIndex with get (): int = _prevIndex
+        member this.Players with get() = _players
+        member this.ReservedCards with get() = _reservedCards
+
+type GameRoom(id) = 
+    inherit GameRoomMetaData() 
+
+    static member Create(id: Guid) = 
+     GameRoom(id)
+
+    // Create, Shuffle, Deal , and leave 3 cards for the LandLord
+    static member Prepare()  = 
+        let room = Guid() |> GameRoom.Create 
+        let cards = Facade.createFullCards() |>  Facade.shuffle 
+        let (reserved, (cards1, cards2, cards3)) = Facade.deal cards
+        // change PlayingCard list to IList<PlayingCard>
+        let convertCardList (cards: PlayingCard list) : IList<PlayingCard> =
+            let result = new List<PlayingCard>(cards)
+            result :> IList<_>
+        (room:>IGameRoomMetaData).Cards.[0] <- convertCardList cards1
+        (room:>IGameRoomMetaData).Cards.[1] <- convertCardList cards2
+        (room:>IGameRoomMetaData).Cards.[2] <- convertCardList cards3
+        room.ReservedCards <- (reserved |> convertCardList)
+        room
+
+    static member FromMetaData(data : IGameRoomMetaData) : GameRoom =
+        let room = GameRoom(data.Id)
+        room.LandLordIndex <- data.LandLordIndex 
+        room.CurrentTurn <- data.CurrentTurn
+        room.Cards <- data.Cards
+        room.Players <- data.Players
+        room.PrevCards <- data.PrevCards
+        room.PrevIndex <- data.PrevIndex
+        room 
+
+     member this.ExportMetaData() = 
+         let origin = this :> IGameRoomMetaData
+         origin   
+
+type GameRoom with 
+
+    member this.AddUser(player: Player) = 
+        let count = (this:> IGameRoomMetaData).Players.Count
+        if count < 3 then
+            (this:>IGameRoomMetaData).Players.Add(player)
+            true
+        else 
+            false
+
+    member this.AppendCards(cards: IList<PlayingCard>) = 
+        let self = this:>IGameRoomMetaData
+        let originalCards = self.Cards.[self.LandLordIndex]
+        for c in cards do
+            originalCards.Add(c)
+
+
+type GameRoom with
+
+    member private this.playCards(nth: int , cards: IList<PlayingCard>) : bool = 
+        if nth < 3 && nth >= 0 then 
+            let originalCards = (this:>IGameRoomMetaData).Cards.[nth]
+            let remaining = originalCards.Except(cards).ToList()
+            
+            (this:>IGameRoomMetaData).Cards.[nth] <- remaining
+            this.PrevIndex <- nth
+            this.PrevCards <- cards
+            this.CurrentTurn <- (this:>IGameRoomMetaData).CurrentTurn + 1
+            true
+        else 
+            failwith "the nth must be an int between [0,2]"
+
+    member this.StartPlayingCards (cards: IList<PlayingCard>) : bool= 
+        let turn = 0 
+        if List.ofSeq cards |> Facade.canStartPlaying then
+            this.playCards(turn, cards ) 
+        else
+            false
+
+    // nth player plays cards
+    member this.PlayCards (nth: int, cards:IList<PlayingCard>) : bool= 
+        let prevCards = (this:>IGameRoomMetaData).PrevCards |> List.ofSeq
+        if List.ofSeq cards |> Facade.canPlay prevCards then
+            this.playCards(nth, cards)
+        else
+            false
+
+    // nth player passes by
+    member this.Pass(nth: int, cards:IList<PlayingCard>) : bool= 
+        this.CurrentTurn <- (this:>IGameRoomMetaData).CurrentTurn + 1
+        true
+
+    member this.HasWin (nth: int) : bool = 
+        let cards = (this:>IGameRoomMetaData).Cards.[nth]
+        cards.Count = 0
+
---
+++ Landlord.Engine.Test/TestGameRoom.fs
@@ -0,0 +1,122 @@
+﻿module TestGameRoom
+
+open System
+open Xunit
+open System.Collections.Generic
+open Itminus.LandLord.Engine.Card
+open Itminus.LandLord.Engine
+open System.Linq
+
+
+
+
+[<Fact>]
+let ``测试GameRoom::Prepare() Cards 各个分组的长度`` () =
+    let gameRoom = GameRoom.Prepare() 
+
+    let gameRoom' = gameRoom :> IGameRoomMetaData
+    // every player has 17 cards
+    for p in gameRoom'.Cards do
+        Assert.Equal(17, p.Count)
+
+    // and reserve 3 cards for landlord
+    let count2 = (gameRoom:>IGameRoomMetaData).ReservedCards.Count
+    Assert.Equal(3,count2)
+
+let private getActualCardsSet (includingReserved:bool) (gameRoom: IGameRoomMetaData) =
+    let cards1' = gameRoom.Cards.[0] |> Set.ofSeq
+    let cards2' = gameRoom.Cards.[1] |> Set.ofSeq 
+    let cards3' = gameRoom.Cards.[2] |> Set.ofSeq
+    let reserved' = gameRoom.ReservedCards |> Set.ofSeq 
+    let set= 
+        cards1' 
+        |> Set.union cards2' 
+        |> Set.union cards3' 
+    if includingReserved then 
+        set |> Set.union reserved'
+    else 
+        set
+
+[<Fact>]
+let ``测试GameRoom::Prepare() Cards的完备性和唯一性`` () =
+
+    let fullCardsSet = Facade.createFullCards() |> Set.ofList
+
+    let originGameRoom = GameRoom.Prepare()
+
+    let gameRoom = originGameRoom :> IGameRoomMetaData
+    Assert.Equal(17, gameRoom.Cards.[0].Count)
+    Assert.Equal(17, gameRoom.Cards.[1].Count)
+    Assert.Equal(17, gameRoom.Cards.[2].Count)
+    Assert.Equal(3,  gameRoom.ReservedCards.Count)
+
+    let actualCardsSet = getActualCardsSet true gameRoom 
+    Assert.Equal(54, actualCardsSet.Count)
+
+    Set.isSubset fullCardsSet actualCardsSet |> Assert.True
+    Set.isSubset fullCardsSet actualCardsSet |> Assert.True
+
+
+let private createTestPlayers() = 
+    let createTestPlayer name = 
+        let p =  Player()
+        p.Name <- name
+        p
+    let p1 = createTestPlayer "player1"
+    let p2 = createTestPlayer "player2"
+    let p3 = createTestPlayer "player3"
+    [p1; p2; p3]
+
+[<Fact>]
+let ``test ::AddPlayer()`` () =
+    let room = GameRoom.Prepare()
+    let players = createTestPlayers()
+    for p in players do
+        room.AddUser p |> Assert.True
+    Assert.Equal(3, (room:>IGameRoomMetaData).Players.Count)
+
+let private createRoomAndAddUserAndSetLandLord landlordIndex = 
+    let room = GameRoom.Prepare()
+    let players = createTestPlayers()
+    for p in players do
+        room.AddUser p |> Assert.True
+    let landlordIndex = 2
+    room.LandLordIndex <- landlordIndex
+    room.AppendCards (room:>IGameRoomMetaData).ReservedCards
+    room 
+
+[<Fact>]
+let ``test GameRoom::AppendCards`` () =
+    let landlordIndex = 2
+    let room = createRoomAndAddUserAndSetLandLord landlordIndex
+    // landlord index
+    Assert.Equal(landlordIndex,(room :> IGameRoomMetaData).LandLordIndex)
+    // reserved cards count
+    Assert.Equal(3, (room:>IGameRoomMetaData).ReservedCards.Count)
+    // cards count
+    for index in [0..2] do
+        if index <> landlordIndex then 
+            Assert.Equal(17, (room:>IGameRoomMetaData).Cards.[index].Count)
+        else 
+            Assert.Equal(20, (room:>IGameRoomMetaData).Cards.[landlordIndex].Count)
+    // players count
+    Assert.Equal(3, (room:>IGameRoomMetaData).Players.Count)
+
+    let actualCardsSet = getActualCardsSet false room
+    Assert.Equal(54, actualCardsSet.Count)
+    let fullCardsSet = Facade.createFullCards() |> Set.ofList
+    Set.isSubset fullCardsSet actualCardsSet |> Assert.True
+    Set.isSubset fullCardsSet actualCardsSet |> Assert.True
+
+
+[<Fact>]
+let ``test GameRoom::StartPlayingCards()`` () =
+    let landlordIndex = 2
+    let room = createRoomAndAddUserAndSetLandLord landlordIndex
+    let cards = (room:>IGameRoomMetaData).Cards
+    let currentTurn = (room:>IGameRoomMetaData).CurrentTurn
+    let playingCards = cards.[currentTurn].Take(1).ToList()
+    room.StartPlayingCards(playingCards) |> Assert.True
+    let empty = cards.[currentTurn].Intersect(playingCards)
+    Assert.Equal(0, empty.Count())
+
