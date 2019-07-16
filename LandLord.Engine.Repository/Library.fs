1a4d116e2ccfd2f07ef3d3980891106ca5021ff7
newbienewbie <itminus@hotmail.com>
2019-07-17 06:50:09 +0800

    Add a thin repository layer
    F# expose quite a lot of abstract class.
    The GameRoomMetaData using quite a lot of types/records/ DUs.
    They cannot be initialized directly by EFCore.
    Also they cannot be retrieved by LiteDb directly.
    
    This is the reason why I add another thin layer for easy usage in C#

---
+++ LandLord.Engine.Repository/Library.fs
@@ -0,0 +1,29 @@
+﻿namespace LandLord.Engine.Repository
+
+open LiteDB
+open LiteDB.FSharp
+open Itminus.LandLord.Engine
+open System
+
+type GameRoomRepository(dbName) =
+    
+    let mapper = FSharpBsonMapper()
+    let dbName:string = dbName   
+    let roomCollectionName: string = "rooms"
+
+    member this.Load(id: Guid)=
+        use db = new LiteDatabase(dbName, mapper)
+        let roomCollection = db.GetCollection<GameRoom>(roomCollectionName);
+        let id' = BsonValue id
+        roomCollection.FindById(id')
+        
+    member this.Save(room: GameRoom) =
+        use db = new LiteDatabase(dbName, mapper)
+        let roomCollection = db.GetCollection<GameRoom>(roomCollectionName);
+        roomCollection.Upsert(room)
+        
+    member this.FindAll () =
+        use db = new LiteDatabase(dbName, mapper)
+        let roomCollection = db.GetCollection<GameRoom>(roomCollectionName);
+        roomCollection.FindAll()
+
