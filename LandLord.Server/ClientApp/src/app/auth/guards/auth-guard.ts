a6babbfc8c7cbd86e51e44c9303b1d033a2eab12
newbienewbie <itminus@hotmail.com>
2019-08-07 15:51:25 +0800

    remove unnecessary console logs

--- LandLord.Server/ClientApp/src/app/auth/guards/auth-guard.ts
+++ LandLord.Server/ClientApp/src/app/auth/guards/auth-guard.ts
@@ -12,17 +12,14 @@ export class AuthGuard implements CanActivate {
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         let isAuthenticated = this.authenticationService.isAuthenticated();
-        console.log(`xxxxxxxxxxxxx`,isAuthenticated);
         if (isAuthenticated) {
             return true;
         }
         return this.authenticationService.login()
             .pipe(map(o =>{
-                console.log(`sssssssssssssssssssssssssssss`,o);
                 if(this.authenticationService.isAuthenticated()){
                     return false;
                 }
-                console.log(`not authenticated, now redirect to login page`);
                 this.router.navigate(['/identity/account/login'], { queryParams: { returnUrl: state.url } });
                 return false;
             }));
