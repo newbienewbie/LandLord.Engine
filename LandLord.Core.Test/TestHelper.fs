b278707db5df68ab6e56215d106e563dde5f8728
newbienewbie <itminus@hotmail.com>
2019-11-14 05:10:56 +0800

    refactor lianshun func for TestHelper

---
+++ LandLord.Core.Test/TestHelper.fs
@@ -0,0 +1,28 @@
+﻿namespace LandLord.Core.Tests
+
+open System
+open LandLord.Core
+open LandLord.Core.Patterns
+open System.Linq
+
+module TestHelper = 
+    
+    // get len of pattern DuoLianShun
+    let lianshun (dup: int) (cards': PlayingCard list) :int = 
+
+        let cards = List.toSeq(cards').ToList()
+
+        let check dup len =
+            let f, _ = Patterns.DuoLianShun(dup, len, cards).ToTuple()
+            f
+
+        let getLen dup = 
+            let mutable len = 0
+            // 三连 ~ 十二连
+            for i= 12 downto 3 do
+                if check dup i then 
+                    len <- i
+            len
+
+        getLen dup
+
--- LandLord.Core.Test/TestLianDui.fs
+++ LandLord.Core.Test/TestLianDui.fs
@@ -7,26 +7,7 @@ module TestsLianDui=
     open LandLord.Core
     open LandLord.Core.Patterns
     open System.Linq
-
-    // get len of pattern DuoLianShun
-    let private lianshun (dup: int) (cards': PlayingCard list) :int = 
-
-        let cards = List.toSeq(cards').ToList()
-
-        let check dup len =
-            let f, _ = Patterns.DuoLianShun(dup, len, cards).ToTuple()
-            f
-
-        let getLen dup = 
-            let mutable len = 0
-            // 三连 ~ 十二连
-            for i= 12 downto 3 do
-                if check dup i then 
-                    len <- i
-            len
-
-        getLen dup
-
+    open TestHelper
 
     [<Fact>]
     let ``测试5连对-顺序`` () =
